3e97589d488247bf8e2aac08f788ec72b892603f
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-05-23 15:25:07 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,7 +1,10 @@
 class Appointment
   attr_accessor :patient, :date, :doctor
-  
+
   @@all = []
 
   def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
 end
