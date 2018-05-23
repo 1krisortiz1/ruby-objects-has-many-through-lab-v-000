6c3320afc3655e574babf30e8d0915f2738858a0
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-05-23 15:13:14 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,9 @@
 class Patient
 
-  @@all = []
+  @@all = []  #class variable that begins as an empty array
+
+
+  def self.all #class method that lists each partient in the class variable
+    @@all
+  end
 end
